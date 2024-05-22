60cfe99bf611552388672247ea694bce1914fa3a
flavio.santana <9090flavio@gmail.com>
2024-05-22 19:08:42 -0300

    #wip: Elegância do teste

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -10,4 +10,25 @@ describe(UniqueIdService.name, () => {
         expect(idWithPrefix.startsWith('APP-')).toBeTrue();
     });
 
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} Deveria gerar um ID unicio quando chamado varias vezes`, () => {
+
+        const service = new UniqueIdService();
+        const firstID = service.generateUniqueIdWithPrefix('APP');
+        const secondID = service.generateUniqueIdWithPrefix('APP');
+
+        expect(firstID).not.toBe(secondID);
+    });
+
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} Deveria gerar um ID unicos quando chamado N vezes`, () => {
+
+        const service = new UniqueIdService();
+
+        const ids = new Set();
+        for (let i = 0; i < 50; i++) {
+            ids.add(service.generateUniqueIdWithPrefix('APP'));
+        }
+
+        expect(ids.size).toBe(50)
+    });
+
 });
