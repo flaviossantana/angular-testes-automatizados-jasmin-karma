fa76578d94ed1653a44f8024a948f2300199a6a1
flavio.santana <9090flavio@gmail.com>
2024-08-07 18:59:35 -0300

    #wip: LikeWidgetComponent

---
+++ src/app/shared/services/like-widget/like-widget.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit, Output} from '@angular/core';
+import {EventEmitter} from "protractor";
+
+@Component({
+    selector: 'app-like-widget',
+    templateUrl: './like-widget.component.html',
+    styleUrls: ['./like-widget.component.scss']
+})
+export class LikeWidgetComponent implements OnInit {
+
+    @Output() public liked = new EventEmitter<void>();
+    @Input() public likes = 0;
+    @Input() public id = null;
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
---
+++ src/app/shared/services/like-widget/like-widget.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {LikeWidgetComponent} from "./like-widget.component";
+
+
+@NgModule({
+    declarations: [LikeWidgetComponent],
+    imports: [
+        CommonModule,
+        FontAwesomeModule
+    ],
+    exports: [LikeWidgetComponent]
+})
+export class LikeWidgetModule {
+}
