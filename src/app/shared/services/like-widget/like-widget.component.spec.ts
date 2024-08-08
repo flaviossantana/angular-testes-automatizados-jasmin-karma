581bff76abc0c72c77103b44830f73d89f6fa6d2
flavio.santana <9090flavio@gmail.com>
2024-08-08 19:22:27 -0300

    #wip: O papel do TestBed

--- src/app/shared/services/like-widget/like-widget.component.spec.ts
+++ src/app/shared/services/like-widget/like-widget.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
 
 describe('LikeWidgetComponent', () => {
+
   let component: LikeWidgetComponent;
   let fixture: ComponentFixture<LikeWidgetComponent>;
 
@@ -13,13 +14,13 @@ describe('LikeWidgetComponent', () => {
     .compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LikeWidgetComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(LikeWidgetComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    // expect(component).toBeTruthy();
   });
 });
