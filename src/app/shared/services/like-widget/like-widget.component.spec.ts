59e7fdc1506993bdc33393b787811a5f68c116d4
flavio.santana <9090flavio@gmail.com>
2024-08-19 19:17:59 -0300

    #wip: Criando um instância do componente

--- src/app/shared/services/like-widget/like-widget.component.spec.ts
+++ src/app/shared/services/like-widget/like-widget.component.spec.ts
@@ -4,25 +4,25 @@ import {LikeWidgetComponent} from './like-widget.component';
 import {UniqueIdService} from "../unique-id/unique-id.service";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
-describe('LikeWidgetComponent', () => {
+describe(LikeWidgetComponent.name, () => {
 
-  let component: LikeWidgetComponent;
-  let fixture: ComponentFixture<LikeWidgetComponent>;
+    let component: LikeWidgetComponent;
+    let fixture: ComponentFixture<LikeWidgetComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ],
-      providers: [UniqueIdService],
-      imports: [FontAwesomeModule]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LikeWidgetComponent],
+            providers: [UniqueIdService],
+            imports: [FontAwesomeModule]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(LikeWidgetComponent);
+        fixture = TestBed.createComponent(LikeWidgetComponent);
 
-  });
+    });
 
-  it('should create', () => {
-    component = fixture.componentInstance;
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        component = fixture.componentInstance;
+        expect(component).toBeTruthy();
+    });
 });
