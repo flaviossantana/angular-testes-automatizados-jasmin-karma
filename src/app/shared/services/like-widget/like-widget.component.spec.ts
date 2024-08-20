4a5b3eb25bc7060ed403b12fe8029910e258a2d0
flavio.santana <9090flavio@gmail.com>
2024-08-20 18:12:21 -0300

    #wip: duas abordagem de teste

--- src/app/shared/services/like-widget/like-widget.component.spec.ts
+++ src/app/shared/services/like-widget/like-widget.component.spec.ts
@@ -1,8 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {LikeWidgetComponent} from './like-widget.component';
-import {UniqueIdService} from "../unique-id/unique-id.service";
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {LikeWidgetModule} from "./like-widget.module";
 
 describe(LikeWidgetComponent.name, () => {
 
@@ -11,9 +10,7 @@ describe(LikeWidgetComponent.name, () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [LikeWidgetComponent],
-            providers: [UniqueIdService],
-            imports: [FontAwesomeModule]
+            imports: [LikeWidgetModule]
         })
             .compileComponents();
 
