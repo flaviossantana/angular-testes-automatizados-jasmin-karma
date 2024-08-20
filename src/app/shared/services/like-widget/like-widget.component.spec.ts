be1b65752916e857ed53ee3f69f1627494dc8024
flavio.santana <9090flavio@gmail.com>
2024-08-20 18:48:56 -0300

    #wip 0017: Conhecendo o Spy

--- src/app/shared/services/like-widget/like-widget.component.spec.ts
+++ src/app/shared/services/like-widget/like-widget.component.spec.ts
@@ -35,13 +35,11 @@ describe(LikeWidgetComponent.name, () => {
         expect(component.id).toBe(someID)
     });
 
-    it(`#${LikeWidgetComponent.prototype.like.name} should trigger emission when called`,  done => {
+    it(`#${LikeWidgetComponent.prototype.like.name} should trigger emission when called`,  () => {
+        spyOn(component.liked, 'emit');
         fixture.detectChanges();
-        component.liked.subscribe(() => {
-            expect(true).toBeTrue();
-            done();
-        })
-        component.like()
+        component.like();
+        expect(component.liked.emit).toHaveBeenCalled();
     });
 
 });
