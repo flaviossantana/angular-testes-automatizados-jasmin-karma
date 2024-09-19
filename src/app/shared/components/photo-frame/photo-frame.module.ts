f66e4f9eecc90683569a80f76f549830860e5136
flavio.santana <9090flavio@gmail.com>
2024-09-19 18:59:35 -0300

    #wip 0104: O módulo photoframe

---
+++ src/app/shared/components/photo-frame/photo-frame.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LikeWidgetModule} from '../../services/like-widget/like-widget.module';
+import {PhotoFrameComponent} from './photo-frame.component';
+
+
+@NgModule({
+    declarations: [PhotoFrameComponent],
+    imports: [
+        CommonModule,
+        LikeWidgetModule
+    ],
+    exports: [PhotoFrameComponent]
+})
+export class PhotoFrameModule {
+}
