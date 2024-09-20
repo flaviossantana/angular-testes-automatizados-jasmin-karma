73db13580296196a9c016926563f729db26e90ec
flavio.santana <9090flavio@gmail.com>
2024-09-20 18:41:23 -0300

    #wip 0107: Um requisito especial

--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,21 +1,38 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {Subject} from 'rxjs';
+import {debounceTime, takeUntil} from 'rxjs/operators';
 
 @Component({
     selector: 'app-photo-frame',
     templateUrl: './photo-frame.component.html',
     styleUrls: ['./photo-frame.component.scss']
 })
-export class PhotoFrameComponent {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
 
-    @Input() descricao = '';
     @Input() source = '';
     @Input() likes = 0;
+    @Input() descricao = '';
 
     @Output() liked: EventEmitter<void> = new EventEmitter();
 
+    debouncedSubject: Subject<void> = new Subject();
+    unSubscribe: Subject<void> = new Subject();
+
+    ngOnInit(): void {
+        this.debouncedSubject
+            .asObservable()
+            .pipe(debounceTime(500))
+            .pipe(takeUntil(this.unSubscribe))
+            .subscribe(debounced => this.liked.emit(debounced));
+    }
+
+    ngOnDestroy(): void {
+        this.unSubscribe.next();
+        this.unSubscribe.complete();
+    }
 
     like(): void {
-        this.liked.emit();
+        this.debouncedSubject.next();
     }
 
 }
